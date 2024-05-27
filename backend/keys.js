debe9d045b08507666a10902335d4d6184d6aba1
proyectos-developer <proyectos@developer-ideas.com>
2024-05-27 09:54:11 -0500

    V1.0.0

---
+++ backend/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'developer-ideas.com',
+        user: 'admin_hospital', 
+        password: 'sngS93#78', 
+        database: 'admin_hospital' 
+    }
+}
